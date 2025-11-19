65ca1bcd63143522b7a55ad515013cb76dfa648a
sena <asena@duck.com>
2025-11-20 01:23:44 +0700

    feat: add preWorkflowChecks to clipsGenerationWorkflow

--- enjin/clips.ts
+++ enjin/clips.ts
@@ -2,7 +2,7 @@ import { v } from "convex/values";
 
 import { workflow } from ".";
 import { internal } from "./_generated/api";
-import { internalMutation, mutation } from "./_generated/server";
+import { internalMutation, internalQuery, mutation } from "./_generated/server";
 
 function workflowLogger(clipsId: string, ...anyColorYouLike: unknown[]) {
   console.info(`[${clipsId}]`, ...anyColorYouLike);
@@ -14,44 +14,77 @@ export const clipsGenerationWorkflow = workflow.define({
     youtubeURL: v.string()
   },
   handler: async (step, args) => {
-    // ############################################### DOWNLOADING THE VIDEO
-    const videoPath = await step.runAction(internal.steps.download, {
+    let captions = null;
+
+    const result = await step.runQuery(internal.clips.preWorkflowChecks, {
       clipsId: args.clipsId,
       youtubeURL: args.youtubeURL
     });
-    workflowLogger(args.clipsId, "succesfuly download video", videoPath);
+    workflowLogger(args.clipsId, "succesfuly checked existence", result.exists);
 
-    const [audioPath] = await Promise.all([
-      // ##################################################### EXTRACT AUDIO
-      step.runAction(internal.steps.extractAudio, {
+    if (!result.exists) {
+      const videoPath = await step.runAction(internal.steps.download, {
         clipsId: args.clipsId,
-        filepath: videoPath
-      }),
-      // ############################################# EXTRACTING MEDIA INFO
-      step.runAction(internal.steps.extractMediaInfo, {
-        clipsId: args.clipsId,
-        filepath: videoPath
-      })
-    ]);
-    workflowLogger(args.clipsId, "succesfuly extracted media info");
-    workflowLogger(args.clipsId, "succesfuly extracted audio", audioPath);
+        youtubeURL: args.youtubeURL
+      });
+      workflowLogger(args.clipsId, "succesfuly download video", videoPath);
 
-    // ################################################### TRANSCRIBE AUDIO
-    const captions = await step.runAction(internal.steps.transcribeAudio, {
+      const [audioPath] = await Promise.all([
+        step.runAction(internal.steps.extractAudio, {
+          clipsId: args.clipsId,
+          filepath: videoPath
+        }),
+        step.runAction(internal.steps.extractMediaInfo, {
+          clipsId: args.clipsId,
+          filepath: videoPath
+        })
+      ]);
+      workflowLogger(args.clipsId, "succesfuly extracted media info");
+      workflowLogger(args.clipsId, "succesfuly extracted audio", audioPath);
+
+      captions = await step.runAction(internal.steps.transcribeAudio, {
+        clipsId: args.clipsId,
+        filepath: audioPath
+      });
+      workflowLogger(args.clipsId, "succesfuly transcribed audio");
+    }
+
+    const clips = await step.runAction(internal.steps.analyzeTranscription, {
       clipsId: args.clipsId,
-      filepath: audioPath
+      captions
     });
-    workflowLogger(args.clipsId, "succesfuly transcribed audio");
-
-    // ############################################## ANALYZE TRANSCRIPTION
-    const analyzedTranscription = await step.runAction(
-      internal.steps.analyzeTranscription,
-      {
-        clipsId: args.clipsId,
-        captions
-      }
-    );
     workflowLogger(args.clipsId, "succesfuly analyzed transcription");
+  }
+});
+
+//check if the video already have transcriptions
+export const preWorkflowChecks = internalQuery({
+  args: {
+    clipsId: v.id("clips"),
+    youtubeURL: v.string()
+  },
+  returns: v.object({
+    exists: v.boolean(),
+    captions: v.optional(v.any())
+  }),
+  handler: async (ctx, args) => {
+    let result = false;
+    let captions;
+
+    const clip = await ctx.db
+      .query("clips")
+      // search for the clip by youtubeURL
+      .filter((q) => q.eq(q.field("youtubeURL"), args.youtubeURL))
+      .first();
+
+    if (!clip || !clip?.captions || !clip?.captions.length) {
+      result = false;
+    } else {
+      result = true;
+      captions = clip.captions;
+    }
+
+    return { exists: result, captions };
   }
 });
 
@@ -89,6 +122,7 @@ export const kickstartClipsGenerationWorkflow = mutation({
   }
 });
 
+//generic patch for clips table
 export const patch = internalMutation({
   args: {
     id: v.id("clips"),
